cc29f7cfa55a563b310d7a5e45f96e883c04a978
sagininuel <sagininuel@gmail.com>
2019-08-14 15:00:26 +0300

    add endpoint to create a todo

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import db from './db/db';
+import bodyParser from 'body-parser';
 
 // Set up the express app
 const app = express();
+
+//Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
@@ -16,4 +21,30 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
+});
+
+// post todo
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if(!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+ const todo = {
+   id: db.length + 1,
+   title: req.body.title,
+   description: req.body.description
+ }
+ db.push(todo);
+ return res.status(201).send({
+   success: 'true',
+   message: 'todo added successfully',
+   todo
+ })
 });
